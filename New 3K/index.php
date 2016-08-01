edda1779f69a332fe18c797a7ab184f5679dcc10
3kwebdesign <my@3kwebdesign.com>
2016-08-01 22:51:47 +0200

    Oprava hlavicky

--- New 3K/index.php
+++ New 3K/index.php
@@ -30,7 +30,7 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="cs-cz">
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
